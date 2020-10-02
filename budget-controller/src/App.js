b85507707fd7fd642a91fd1047522f38d65ec38f
Daniel <danisan9490@gmail.com>
2020-10-02 16:29:41 +0100

    #3 Add Budget Amount and Error

--- budget-controller/src/App.js
+++ budget-controller/src/App.js
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import BudgetQuestion from './controllers/BudgetQuestion';
 
 function App() {
+
+  const [budget, setBudget] = useState(0);
+  const [AmountLeft, setAmountLeft] = useState(0);
+
   return (
     <div className="container">
       <header>
         <h1>Budget Controller</h1>
 
         <div className="main-content content">
-          <BudgetQuestion />
+          <BudgetQuestion
+            setBudget={setBudget}
+            setAmountLeft={setAmountLeft}
+          />
         </div>
 
       </header>
--- budget-controller/src/controllers/BudgetQuestion.js
+++ budget-controller/src/controllers/BudgetQuestion.js
@@ -1,21 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Error from './Error'
 
-function BudgetQuestion() {
+const BudgetQuestion = ({ setBudget, setAmountLeft }) => {
+
+  const [amount, setAmount] = useState(0);
+  const [error, setError] = useState(false);
+
+  const defineBudget = e => {
+    setAmount(parseInt(e.target.value))
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (amount < 1 || isNaN(amount)) {
+      setError(true);
+      return;
+    }
+    setError(false);
+    setBudget(amount);
+    setAmountLeft(amount);
+
+  }
   return (
     <div>
       <h2>Insert your Budget</h2>
+      {error ? <Error message="Invalid amount" /> : null}
 
-      <form>
+      <form
+        onSubmit={handleSubmit}
+      >
         <input
           type="number"
           className="u-full-width"
           placeholder="Insert your Budget..."
+          onChange={defineBudget}
         />
 
         <input
           type="submit"
           className="button-primary u-full-width"
-          value="budget"
+          value="Conitinue"
 
         />
 
