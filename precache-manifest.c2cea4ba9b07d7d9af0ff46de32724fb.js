d279000774d9644e5a8bb78fa8a5da7c9d7527c6
Daniel <danisan9490@gmail.com>
2020-10-03 11:28:28 +0100

    Updates

---
+++ precache-manifest.c2cea4ba9b07d7d9af0ff46de32724fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e016717f4b2acd0d99c97a18ae984688",
+    "url": "/budget-controller/index.html"
+  },
+  {
+    "revision": "34d5bc69459f41cf0578",
+    "url": "/budget-controller/static/css/main.002957e1.chunk.css"
+  },
+  {
+    "revision": "78062808ffca3068a41b",
+    "url": "/budget-controller/static/js/2.81473e04.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/budget-controller/static/js/2.81473e04.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34d5bc69459f41cf0578",
+    "url": "/budget-controller/static/js/main.ae18afd8.chunk.js"
+  },
+  {
+    "revision": "dabd405e337936cc2c6f",
+    "url": "/budget-controller/static/js/runtime-main.1cee73da.js"
+  }
+]);
